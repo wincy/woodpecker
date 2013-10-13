2aa9d07d59972490cc717143b7b9adb1f25165e7
Jiang Hailong <gombiuda@gmail.com>
2013-10-13 09:08:15 +0800

    simplify test

--- js/lib/asana/asana.js
+++ js/lib/asana/asana.js
@@ -401,228 +401,148 @@ define("asana", ["jquery", "ember", "when", "when/parallel", "when/sequence", "w
 	},
     });
 
-    Asana.test = function() {
-    	QUnit.asyncTest("Asana testing", function() {
-    	    when.pipeline([
-    		Asana.Workspace.sync,
-    		Asana.Workspace.find,
-    		function(workspaces) {
-    		    return when.all(when.map(workspaces, function(workspace) {
-    			return workspace.sync();
-    		    }));
-    		},
-    		function() {
-    		    return Asana.Workspace.get({name: 'Test'});
-    		},
-    		function(workspace) {
-    		    ok(workspace, "Workspace 'Test' exists");
-    		    return when.pipeline([
-    			workspace.User.sync,
-			workspace.User.find,
-			function(items) {
-			    return when.all(when.map(items, function(item) {
-				return when.pipeline([
-				    item.sync.bind(item),
-				    item.index.bind(item),
-				]);
-			    }))
-			},
-    			workspace.Project.sync,
-			workspace.Project.find,
-			function(items) {
-			    return when.all(when.map(items, function(item) {
-				return when.pipeline([
-				    item.sync.bind(item),
-				    item.index.bind(item),
-				]);
-			    }))
-			},
-    			workspace.Task.sync,
-			workspace.Task.find,
-			function(items) {
-			    return when.all(when.map(items, function(item) {
-				return when.pipeline([
-				    item.sync.bind(item),
-				    item.index.bind(item),
-				]);
-			    }))
-			},
-    			workspace.Tag.sync,
-			workspace.Tag.find,
-			function(items) {
-			    return when.all(when.map(items, function(item) {
-				return when.pipeline([
-				    item.sync.bind(item),
-				    item.index.bind(item),
-				]);
-			    }))
-			},
-    		    ]).then(function() {
-    			return workspace;
-    		    });
-    		},
-    		function(workspace) {
-    		    return when.reduce([
-    			when.reduce(when.map(workspace.Project.find(), function(project) {
-    			    return when.pipeline([
-    				project.sync.bind(project), // `this` will be lost in when.sequence
-    				project.Task.sync,
-    				project.Task.find,
-    			    ]);
-    			}), array_concat, []),
-    			when.reduce(when.map(workspace.Tag.find(), function(tag) {
-    			    return when.pipeline([
-    				tag.sync.bind(tag), // `this` will be lost in when.sequence
-    				tag.Task.sync,
-    				tag.Task.find,
-    			    ]);
-    			}), array_concat, []),
-    		    ], array_concat, []).then(function(tasks) {
-    			return tasks.reduce(function(s, task) {
-    			    if (s.indexOf(task.id) == -1) {
-    				s.push(task.id);
-    			    }
-    			    return s;
-    			}, []).map(function(id) {
-    			    return Asana.Task.create({id: id});
-    			});
-    		    }).then(function(tasks) {
-    			return when.all(when.map(tasks, function(task) {
-    			    return task.sync();
-    			}));
-    		    }).then(function() {
-    			return workspace;
-    		    });
-    		},
-    		function(workspace) {
-    		    when.pipeline([
-    			// check project
-    			function() {
-    			    return true;
-    			},
-    			workspace.Project.find,
-    			function(projects) {
-    			    equal(projects.length, 1, "Only 1 project exists");
-    			    equal(projects[0].name, "project 0", "project name is 'project 0'");
-    			    return projects[0];
-    			},
-    			// check tasks
-    			function(project) {
-    			    return when.pipeline([
-    				function () {
-    				    return true;
-    				},
-    				project.Task.find,
-    				function(tasks) {
-    				    var names = tasks.map(function(task) {
-    					return task.name;
-    				    });
-    				    deepEqual(names.sort(),
-    					      ['task 0',
-    					       'task 0 subtask 0',
-    					       'task 0 subtask 1',
-    					       'task 0 subtask 2',
-    					       'task 0 subtask 1 subtask 0',
-    					       'task 0 subtask 1 subtask 1'].sort());
-    				},
-    			    ]);
-    			},
-    			// check tags
-    			function() {
-    			    return true;
-    			},
-			workspace.Tag.sync,
-    			workspace.Tag.find,
-    			function(tags) {
-    			    equal(tags.length, 3, 'Only 3 tags exist');
-    			    var names = tags.map(function(tag) {
-    				return tag.name;
-    			    });
-    			    deepEqual(names.sort(),
-    				      ['tag 0', 'tag 1', 'tag 2'].sort(),
-    				      "tag list correct");
-    			    // check tag tasks
-    			    tag_tasks = {'tag 0': ['task 0'],
-    					 'tag 1': ['task 0 subtask 0',
-    						   'task 0 subtask 1 subtask 0'],
-    					 'tag 2': ['task 0 subtask 1',
-    						   'task 0 subtask 1 subtask 1']}
-    			    return when.all(when.map(tags, function(tag) {
-    			    	return when.pipeline([
-    			    	    function() {
-    			    		return true;
-    			    	    },
-    			    	    tag.Task.find,
-    			    	    function(tasks) {
+    Asana.reopen({
+	test: function() {
+    	    QUnit.asyncTest("Asana testing", function() {
+    		when.pipeline([
+		    Asana.sync,
+    		    function() {
+    			return Asana.Workspace.get({name: 'Test'})
+			    .then(function(workspace) {
+    				ok(workspace, "Workspace 'Test' exists");
+				return workspace;
+			    });
+    		    },
+    		    function(workspace) {
+    			when.pipeline([
+    			    // check project
+    			    function() {
+    				return true;
+    			    },
+    			    workspace.Project.find,
+    			    function(projects) {
+    				equal(projects.length, 1, "Only 1 project exists");
+    				equal(projects[0].name, "project 0", "project name is 'project 0'");
+    				return projects[0];
+    			    },
+    			    // check tasks
+    			    function(project) {
+    				return when.pipeline([
+    				    function () {
+    					return true;
+    				    },
+    				    project.Task.find,
+    				    function(tasks) {
     					var names = tasks.map(function(task) {
     					    return task.name;
     					});
-    					deepEqual(
-    					    names.sort(),
-    					    tag_tasks[tag.name].sort(),
-    					    sprintf("tag '%s' task list correct",
-    						    tag.name));
-    			    	    }
-    			    	]);
-    			    }));
-    			},
-			function() {
-			    return workspace.Tag.new({name: 'tag 4'})
-				.then(function(tag) {
-				    return workspace.Task.get({name: 'task 0'})
-					.then(function(task) {
-					    return task.addTag(tag).then(function() {
-						return task;
+    					deepEqual(names.sort(),
+    						  ['task 0',
+    						   'task 0 subtask 0',
+    						   'task 0 subtask 1',
+    						   'task 0 subtask 2',
+    						   'task 0 subtask 1 subtask 0',
+    						   'task 0 subtask 1 subtask 1'].sort());
+    				    },
+    				]);
+    			    },
+    			    // check tags
+    			    function() {
+    				return true;
+    			    },
+			    workspace.Tag.sync,
+    			    workspace.Tag.find,
+    			    function(tags) {
+    				equal(tags.length, 3, 'Only 3 tags exist');
+    				var names = tags.map(function(tag) {
+    				    return tag.name;
+    				});
+    				deepEqual(names.sort(),
+    					  ['tag 0', 'tag 1', 'tag 2'].sort(),
+    					  "tag list correct");
+    				// check tag tasks
+    				tag_tasks = {'tag 0': ['task 0'],
+    					     'tag 1': ['task 0 subtask 0',
+    						       'task 0 subtask 1 subtask 0'],
+    					     'tag 2': ['task 0 subtask 1',
+    						       'task 0 subtask 1 subtask 1']}
+    				return when.all(when.map(tags, function(tag) {
+    			    	    return when.pipeline([
+    			    		function() {
+    			    		    return true;
+    			    		},
+    			    		tag.Task.find,
+    			    		function(tasks) {
+    					    var names = tasks.map(function(task) {
+    						return task.name;
+    					    });
+    					    deepEqual(
+    						names.sort(),
+    						tag_tasks[tag.name].sort(),
+    						sprintf("tag '%s' task list correct",
+    							tag.name));
+    			    		}
+    			    	    ]);
+    				}));
+    			    },
+			    function() {
+				return workspace.Tag.new({name: 'tag 4'})
+				    .then(function(tag) {
+					return workspace.Task.get({name: 'task 0'})
+					    .then(function(task) {
+						return task.addTag(tag).then(function() {
+						    return task;
+						});
 					    });
-					});
-				});
-			},
-			function(task) {
-			    return when.pipeline([
-				task.Tag.sync,
-				task.Tag.find,
-				function(tags) {
-				    return when.all(when.map(tags, function(tag) {
-					return when.pipeline([
-					    tag.sync.bind(tag),
-					    tag.load.bind(tag),
-					]);
-				    }));
-				},
-				function() {
-				    return task.Tag.get({name: 'tag 4'});
-				},
-				function(tag) {
-				    ok(tag, 'newly added tag');
-				    return tag;
-				},
-				function(tag) {
-				    return task.removeTag(tag);
-				},
-				task.Tag.sync,
-				task.Tag.find,
-				function(tags) {
-				    return when.all(when.map(tags, function(tag) {
-					return when.pipeline([
-					    tag.sync.bind(tag),
-					    tag.load.bind(tag),
-					]);
-				    }));
-				},
-				function() {
-				    return task.Tag.get({name: 'tag 4'});
-				},
-				function(tag) {
-				    ok(tag == null, 'remove added tag');
-				},
-			    ]);
-			},
-    			start,
-    		    ], true);
-    		},
-    	    ]);
-    	});
-    };
+				    });
+			    },
+			    function(task) {
+				return when.pipeline([
+				    task.Tag.sync,
+				    task.Tag.find,
+				    function(tags) {
+					return when.all(when.map(tags, function(tag) {
+					    return when.pipeline([
+						tag.sync.bind(tag),
+						tag.load.bind(tag),
+					    ]);
+					}));
+				    },
+				    function() {
+					return task.Tag.get({name: 'tag 4'});
+				    },
+				    function(tag) {
+					ok(tag, 'newly added tag');
+					return tag;
+				    },
+				    function(tag) {
+					return task.removeTag(tag);
+				    },
+				    task.Tag.sync,
+				    task.Tag.find,
+				    function(tags) {
+					return when.all(when.map(tags, function(tag) {
+					    return when.pipeline([
+						tag.sync.bind(tag),
+						tag.load.bind(tag),
+					    ]);
+					}));
+				    },
+				    function() {
+					return task.Tag.get({name: 'tag 4'});
+				    },
+				    function(tag) {
+					ok(tag == null, 'remove added tag');
+				    },
+				]);
+			    },
+    			    start,
+    			], true);
+    		    },
+    		]);
+    	    });
+	},
+    });
 
     return Asana;
 })
